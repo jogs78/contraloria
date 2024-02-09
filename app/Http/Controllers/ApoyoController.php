3823d124ad6ef555a58ce8ae38bbd8136ad208af
Jorge Octavio Guzmán Sánchez <jogs78@gmail.com>
2024-02-09 17:21:35 -0600

    Modelos creados

---
+++ app/Http/Controllers/ApoyoController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreApoyoRequest;
+use App\Http\Requests\UpdateApoyoRequest;
+use App\Models\Apoyo;
+
+class ApoyoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreApoyoRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Apoyo $apoyo)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Apoyo $apoyo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateApoyoRequest $request, Apoyo $apoyo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Apoyo $apoyo)
+    {
+        //
+    }
+}
